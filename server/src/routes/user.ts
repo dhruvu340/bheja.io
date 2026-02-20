c5b2340ed1d0b5dce12192d77add69614e2c2c58
dhruvu340 <dhruvu3400@gmail.com>
2026-02-20 10:27:30 +0530

    authentication completed

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { Logout, Signin, Signup } from "../controllers/user.js";
+import { Logout, Signin, Signup ,genAccessToken } from "../controllers/user.js";
 
 const authRouter=express.Router();
 
 authRouter.post("/signup",Signup);
 authRouter.post("/signin",Signin);
 authRouter.post("/logout",Logout);
+authRouter.post("/genaccess",genAccessToken);
 
 
 
